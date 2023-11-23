a11d94751a2de3aad13bdfb516efdc1551d2dff9
Peter <peterhamblin@hotmail.co.uk>
2023-11-23 20:04:58 +0000

    Add project files.

---
+++ The Triangle Farmer/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter the triangle's base size: ");
+int baseSize = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the triangle's base height: ");
+int baseHeight = Convert.ToInt32(Console.ReadLine());
+
+int area = baseSize * baseHeight / 2;
+
+Console.WriteLine("The area of the triangle is " + area);
