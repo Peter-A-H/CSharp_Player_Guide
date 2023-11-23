1119e7508c2041de741973aae04922e52de5999c
Peter <peterhamblin@hotmail.co.uk>
2023-11-23 20:11:08 +0000

    Finished challenge

--- The Four Sisters & The Duckbear/Program.cs
+++ The Four Sisters & The Duckbear/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a number of eggs: ");
+int sisters = Convert.ToInt32(Console.ReadLine()) / 4;
+int duckbear = sisters % 4;
+Console.WriteLine("Each of the four sisters get " + sisters + " eggs");
+Console.WriteLine("The duckbear gets the remaining " + duckbear + " eggs");
