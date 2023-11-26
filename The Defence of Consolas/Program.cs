cbb92b8f2ea20e838d10a8bc1cd47fedf8c50373
Peter <peterhamblin@hotmail.co.uk>
2023-11-26 21:17:52 +0000

    Finished Challenge

---
+++ The Defence of Consolas/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Target Row? ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Target Column? ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Deploy to: ");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Beep(440, 500);
+
+Console.WriteLine($"({row}, {--column})");
+Console.WriteLine($"({--row}, {++column})");
+Console.WriteLine($"({++row}, {++column})");
+Console.WriteLine($"({++row}, {--column})");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
---
+++ The Dominion of Kings/Program.cs
@@ -0,0 +1,15 @@
+﻿int totalScore = 0;
+
+Console.WriteLine("How many provinces do you have? ");
+int provinces = Convert.ToInt32(Console.ReadLine());
+totalScore += provinces;
+
+Console.WriteLine("How many duchies do you have? ");
+int duchies = Convert.ToInt32(Console.ReadLine());
+totalScore += duchies * 3;
+
+Console.WriteLine("How many estates do you have? ");
+int estates = Convert.ToInt32(Console.ReadLine());
+totalScore += estates * 6;
+
+Console.WriteLine("Your total score is: " + totalScore);
