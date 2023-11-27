7bbea2bb8073a7c0432065126fe68c597eefe712
Peter <peterhamblin@hotmail.co.uk>
2023-11-27 20:43:58 +0000

    Finished Challenge

---
+++ Repairing the Clocktower/Program.cs
@@ -0,0 +1,13 @@
+﻿while (true)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("Tick");
+    }
+    else
+    {
+        Console.WriteLine("Tock");
+    }
+}
