3f313ec52a01d706a85d390e6d1ea66e1ca7a0cc
Peter <peterhamblin@hotmail.co.uk>
2023-11-28 19:31:10 +0000

    Project completed

---
+++ Watchtower/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Enter a X value: ");
+int xValue = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a Y value: ");
+int yValue = Convert.ToInt32(Console.ReadLine());
+
+if (xValue == 0 && yValue == 0)
+{
+    Console.WriteLine("The enemy is here!");
+}
+else if (xValue < 0 && yValue > 0)
+{
+    Console.WriteLine("The enemy is north-west!");
+}
+else if (xValue == 0 && yValue > 0)
+{
+    Console.WriteLine("The enemy is in the north!");
+}
+else if (xValue > 0 && yValue > 0)
+{
+    Console.WriteLine("The enemy is in the north-east!");
+}
+else if (xValue > 0 && yValue == 0)
+{
+    Console.WriteLine("The enemy is in the west!");
+}
+else if (xValue > 0 && yValue < 0)
+{
+    Console.WriteLine("The enemy is in the south-east!");
+}
+else if (xValue == 0 & yValue < 0)
+{
+    Console.WriteLine("The enemy is in the south!");
+}
+else if (xValue < 0 && yValue < 0)
+{
+    Console.WriteLine("The enemy is in the south-west!");
+}
+else if (xValue < 0 && yValue == 0)
+{
+    Console.WriteLine("The enemy is in the west!");
+}
