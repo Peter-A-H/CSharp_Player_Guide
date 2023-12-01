4669d62a91430e664d5156c192199193196543b9
Peter <peterhamblin@hotmail.co.uk>
2023-12-01 21:40:52 +0000

    Extra Objective

--- The Magic Cannon/Program.cs
+++ The Magic Cannon/Program.cs
@@ -1,15 +1,20 @@
 ﻿for (int i = 1; i < 101; i++)
 {
     string gem = "Normal";
+    Console.ForegroundColor = ConsoleColor.White;
 
     if (i % 3 == 0)
     {
         gem = "Fire";
+        Console.ForegroundColor = ConsoleColor.Red;
     }
     else if (i % 5 == 0)
     {
         gem = "Electric";
+        Console.ForegroundColor = ConsoleColor.Yellow;
     }
 
     Console.WriteLine($"{i}: {gem}");
 }
+
+Console.ForegroundColor = ConsoleColor.White;
