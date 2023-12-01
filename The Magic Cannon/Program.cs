4a624fb95569bb1addd8dadf04224e250e798ae0
Peter <peterhamblin@hotmail.co.uk>
2023-12-01 21:38:44 +0000

    Finished Challenge

---
+++ The Magic Cannon/Program.cs
@@ -0,0 +1,15 @@
+﻿for (int i = 1; i < 101; i++)
+{
+    string gem = "Normal";
+
+    if (i % 3 == 0)
+    {
+        gem = "Fire";
+    }
+    else if (i % 5 == 0)
+    {
+        gem = "Electric";
+    }
+
+    Console.WriteLine($"{i}: {gem}");
+}
