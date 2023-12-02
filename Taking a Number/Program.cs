94a8b701e02029e649cc842b81e9ac2fa77138af
Peter <peterhamblin@hotmail.co.uk>
2023-12-02 17:04:02 +0000

    Practice for multi-dimension arrays

---
+++ Taking a Number/Program.cs
@@ -0,0 +1,11 @@
+﻿int[,] matrix = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int column = 0; column < matrix.GetLength(1); column++)
+    {
+        Console.Write(matrix[row, column] + " ");
+    }
+
+    Console.WriteLine();
+}
---
+++ The Laws of Freach/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentSmallest = int.MaxValue;
+foreach (int num in array)
+{
+    if (num < currentSmallest)
+    {
+        currentSmallest = num;
+    }
+}
+
+int total = 0;
+foreach (int number in array)
+{
+    total += number;
+}
+
+float average = (float)total / array.Length;
+
+Console.WriteLine($"Smallest: {currentSmallest}");
+Console.WriteLine($"Average: {average}");
