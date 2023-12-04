e6c4688553fdaa6e501182a21168161b2a3126a0
Peter <peterhamblin@hotmail.co.uk>
2023-12-04 22:30:14 +0000

    Initial commit

---
+++ Memory/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] a1, a2;
+
+a1 = new int[] { 1, 2, 3 };
+
+a2 = a1;
+
+a2[0] = 100;
+
+Console.WriteLine(a1[0]);
+
+int[] a = new int[] { 1, 2, 3 };
+int[] b = new int[] { 1, 2, 3 };
+
+Console.WriteLine(a == b); // reference semantics, same data but different memory locations on the heap
