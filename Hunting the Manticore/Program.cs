ec2192a96e978ecaaec37c8514bc59d742180d20
Peter <peterhamblin@hotmail.co.uk>
2023-12-05 21:10:08 +0000

    Finished Challenge

--- Hunting the Manticore/Program.cs
+++ Hunting the Manticore/Program.cs
@@ -15,7 +15,7 @@ int CannonDamage()
     if (round % 3 == 0 && round % 5 == 0) return 10;
     else if (round % 5 == 0) return 3;
     else if (round % 3 == 0) return 3;
-    else return 1;
+    return 1;
 }
 
 string CannonDamageType()
@@ -23,7 +23,6 @@ string CannonDamageType()
     if (round % 3 == 0 && round % 5 == 0) return "fire-electric";
     else if (round % 5 == 0) return "electric";
     else if (round % 3 == 0) return "fire";
-
     return "";
 }
 
@@ -33,12 +32,11 @@ void DisplayDamage()
     else Console.WriteLine($"The cannon is expected to deal {CannonDamage()} {CannonDamageType()} damage this round.");
 }
 
-void DisplayHitStatus()
+void DisplayHitStatus(string hitType)
 {
-    string hitType = CannonAccuracy();
-
     if (hitType == "DIRECT HIT!") Console.WriteLine($"That round was a {hitType}");
-    else if (hitType == "OVERSHOT" || hitType == "FELL SHORT") Console.WriteLine($"That round {hitType} the target.");
+    else if (hitType == "FELL SHORT") Console.WriteLine($"That round {hitType} of the target.");
+    else Console.WriteLine($"That round {hitType} the target.");
 }
 
 int PromptCannonRange()
@@ -51,13 +49,17 @@ int PromptCannonRange()
     return range;
 }
 
-string CannonAccuracy()
+string CannonAccuracy(int range)
 {
-    int range = PromptCannonRange();
-
     if (range > manticoreDistance) return "OVERSHOT";
     else if (range < manticoreDistance) return "FELL SHORT";
-    else return "DIRECT HIT!";
+    return "DIRECT HIT!";
+}
+
+void CheckForWinner()
+{
+    if (cityHealth <= 0) Console.WriteLine("The city has been destroyed!");
+    else if (manticoreHealth <= 0) Console.WriteLine("The manticore has been destroyed!");
 }
 
 Console.Write("Player 1, how far away from the city do you want to station the Manticore? ");
@@ -66,12 +68,21 @@ manticoreDistance = Convert.ToInt32(Console.ReadLine());
 Console.Clear();
 
 Console.WriteLine("Player 2, it is your turn.");
-Console.WriteLine("----------------------------------------------------------------------------");
 
-while (cityHealth > 0 || manticoreHealth > 0)
+while (cityHealth > 0 && manticoreHealth > 0)
 {
+    Console.WriteLine("----------------------------------------------------------------------------");
     DisplayStatus();
     DisplayDamage();
-    CannonAccuracy();
-    DisplayHitStatus();
+
+    int range = PromptCannonRange();
+    string accuracy = CannonAccuracy(range);
+
+    DisplayHitStatus(accuracy);
+
+    if (accuracy == "DIRECT HIT!") manticoreHealth -= CannonDamage();
+    ++round;
+    --cityHealth;
+
+    CheckForWinner();
 }
