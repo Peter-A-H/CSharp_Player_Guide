874e8f44f8b2b115b1084adf4e805dfcd2971b8f
Peter <peterhamblin@hotmail.co.uk>
2023-12-16 17:45:50 +0000

    Finished Challenge

---
+++ The Point/Program.cs
@@ -0,0 +1,13 @@
+﻿Point point = new(2, 3);
+Point point1 = new(-4, 0);
+Console.WriteLine($"({point.X}, {point.Y})");
+Console.WriteLine($"({point1.X}, {point1.Y})");
+
+public class Point(int x, int y)
+{
+    public int X { get; } = x;
+    public int Y { get; } = y;
+
+    // Call primary constructor, default to 0, 0 if no params are passed
+    public Point() : this(0, 0) { }
+}
