cc8472af7cac275bd5d67f025c4e611903bee96e
Peter <peterhamblin@hotmail.co.uk>
2023-12-17 14:06:12 +0000

    Challenge Completed

--- The Password Validator/Program.cs
+++ The Password Validator/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var validator = new PasswordValidator("PeterAH1234!");
+Console.WriteLine(validator.Validate());
+
+public class PasswordValidator(string password)
+{
+    public string Password { get; private set; } = password;
+
+    public bool Validate()
+    {
+        bool hasLowerCase = false;
+        bool hasUpperCase = false;
+        bool hasDigit = false;
+
+        if (Password == null || Password.Length < 6 || Password.Length > 13)
+        {
+            return false;
+        }
+
+        foreach (char letter in Password)
+        {
+            if (letter == 'T' || letter == '&')
+            {
+                return false;
+            }
+        }
+
+        foreach (char letter in Password)
+        {
+            if (char.IsUpper(letter)) hasUpperCase = true;
+            if (char.IsLower(letter)) hasLowerCase = true;
+            if (char.IsDigit(letter)) hasDigit = true;
+        }
+
+        return hasUpperCase && hasLowerCase && hasDigit;
+    }
+}
