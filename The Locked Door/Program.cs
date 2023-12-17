d5be6a9ebf5ed6c088d6c086727ff149e30b9a34
Peter <peterhamblin@hotmail.co.uk>
2023-12-17 13:31:32 +0000

    Challenge Completed

--- The Locked Door/Program.cs
+++ The Locked Door/Program.cs
@@ -1,2 +1,94 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var door = new Door(1234);
+door.ChangePasscode(1234, 5555);
+
+Console.WriteLine($"The door is currently {door.State}.");
+door.Unlock();
+
+public class Door(int passcode)
+{
+    public int Passcode { private get; set; } = passcode;
+    public DoorState State { get; private set; } = DoorState.Closed;
+
+    public void ChangePasscode(int currentPasscode, int newPasscode)
+    {
+        if (currentPasscode == Passcode)
+        {
+            Console.WriteLine($"Passcode changed from {Passcode} to {newPasscode}.");
+            Passcode = newPasscode;
+        }
+        else
+        {
+            Console.WriteLine("The current passcode is not correct.");
+        }
+    }
+
+    public void Open()
+    {
+        if (State == DoorState.Locked)
+        {
+            State = DoorState.Open;
+            Console.WriteLine("The door is now open.");
+        }
+        else if (State == DoorState.Open)
+        {
+            Console.WriteLine("The door is already open.");
+        }
+        else
+        {
+            Console.WriteLine("Unable to open the door.");
+        }
+    }
+
+    public void Close()
+    {
+        if (State == DoorState.Open)
+        {
+            State = DoorState.Closed;
+            Console.WriteLine("The door is now closed.");
+        }
+        else if (State == DoorState.Closed)
+        {
+            Console.WriteLine("The door is already closed.");
+        }
+        else
+        {
+            Console.WriteLine("Unable to close the door.");
+        }
+    }
+
+    public void Lock()
+    {
+        if (State == DoorState.Closed)
+        {
+            State = DoorState.Locked;
+            Console.WriteLine("The door is now locked.");
+        }
+        else if (State == DoorState.Locked)
+        {
+            Console.WriteLine("The door is already locked.");
+        }
+        else
+        {
+            Console.WriteLine("Unable to lock the door.");
+        }
+    }
+
+    public void Unlock()
+    {
+        if (State == DoorState.Locked)
+        {
+            State = DoorState.Unlocked;
+            Console.WriteLine("The door is now unlocked.");
+        }
+        else if (State == DoorState.Closed)
+        {
+            Console.WriteLine("The door is already unlocked.");
+        }
+        else
+        {
+            Console.WriteLine("Unable to unlock the door.");
+        }
+    }
+}
+
+public enum DoorState { Locked, Closed, Open, Unlocked }
