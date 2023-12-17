1bb0c08f197aa2df594b50b55430bef265931305
Peter <peterhamblin@hotmail.co.uk>
2023-12-17 21:36:51 +0000

    Created classes and game grid.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var game = new TicTacToeGame();
+var player1 = new Player('X');
+var player2 = new Player('O');
+
+game.Run();
+
+while (game.InPlay)
+{
+    Grid.Draw();
+}
+
+public class Player(char crossOrNaught)
+{
+    public char CrossOrNaught { get; private set; } = crossOrNaught;
+}
+
+static class Grid
+{
+    static public void Draw()
+    {
+        Console.WriteLine($" X | O | X ");
+        Console.WriteLine($"---+---+---");
+        Console.WriteLine($" X | O | O ");
+        Console.WriteLine($"---+---+---");
+        Console.WriteLine($" X | 0 | X ");
+    }
+}
+
+public class TicTacToeGame
+{
+    public bool InPlay { get; private set; }
+
+    public void Run()
+    {
+        InPlay = true;
+    }
+
+    public void End()
+    {
+        InPlay = false;
+    }
+}
