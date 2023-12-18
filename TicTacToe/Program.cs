f1a86b09f91fbb35f81b2a7e1d386d3b59fe3662
Peter <peterhamblin@hotmail.co.uk>
2023-12-18 18:48:28 +0000

    Created win condition matrix

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -27,11 +27,22 @@ public class Player(char crossOrNaught)
 
 public class Grid
 {
-    public char[,] Squares { get; private set; } = new char[3, 3]
+    public static char[,] Squares { get; private set; } = new char[3, 3]
     {
         { '1', '2', '3' },
         { '4', '5', '6' },
         { '7', '8', '9' }
+    };
+    public char[,] WinConditions { get; private set; } = new char[8, 3]
+    {
+        { Squares[0, 0], Squares[0, 1], Squares[0, 2] },
+        { Squares[1, 0], Squares[1, 1], Squares[1, 2] },
+        { Squares[2, 0], Squares[2, 1], Squares[2, 2] },
+        { Squares[0, 0], Squares[1, 0], Squares[2, 0] },
+        { Squares[0, 1], Squares[1, 1], Squares[2, 1] },
+        { Squares[0, 2], Squares[1, 2], Squares[2, 2] },
+        { Squares[0, 0], Squares[1, 1], Squares[2, 2] },
+        { Squares[2, 0], Squares[1, 1], Squares[0, 2] },
     };
 
     public void Draw()
@@ -80,7 +91,6 @@ public class Grid
     }
 }
 
-// Store a 3x3 matrix for the win conditions
 public class TicTacToeGame
 {
     public bool InPlay { get; private set; }
@@ -99,6 +109,4 @@ public class TicTacToeGame
     {
 
     }
-
-    // all 8 possible win conditions
 }
