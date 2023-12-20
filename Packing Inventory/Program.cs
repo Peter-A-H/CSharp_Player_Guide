6bcc18e57e1749c9e6bc927f2de7f534ef271296
Peter <peterhamblin@hotmail.co.uk>
2023-12-20 22:21:17 +0000

    Initial commit, classes defined with base case

---
+++ Packing Inventory/Program.cs
@@ -0,0 +1,29 @@
+﻿public class InventoryItem(float weight, float volume)
+{
+    public float Weight { get; set; } = weight;
+    public float Volume { get; set; } = volume;
+}
+
+public class Arrow(float weight, float volume) : InventoryItem(weight, volume)
+{
+}
+
+public class Rope(float weight, float volume) : InventoryItem(weight, volume)
+{
+
+}
+
+public class Water(float weight, float volume) : InventoryItem(weight, volume)
+{
+
+}
+
+public class Sword(float weight, float volume) : InventoryItem(weight, volume)
+{
+
+}
+
+public class Food(float weight, float volume) : InventoryItem(weight, volume)
+{
+
+}
