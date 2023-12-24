f5bd8c1f52f6aab3762c8198f6432c676c7c214a
Peter <peterhamblin@hotmail.co.uk>
2023-12-24 17:23:34 +0000

    Added Pack and InventoryItem classes

--- Packing Inventory/Program.cs
+++ Packing Inventory/Program.cs
@@ -1,8 +1,24 @@
-﻿public class InventoryItem(float weight, float volume)
+﻿Pack pack = new(10, 15, 20);
+
+public class Pack(int totalItems, float maxWeight, float maxVolume)
+{
+    public InventoryItem[] Items { get; set; } = new InventoryItem[totalItems];
+    public int TotalItems = totalItems;
+    public float MaxWeight = maxWeight;
+    public float MaxVolume = maxVolume;
+
+    public bool Add(InventoryItem item)
+    {
+
+    }
+}
+
+public class InventoryItem(float weight, float volume)
 {
     public float Weight { get; set; } = weight;
     public float Volume { get; set; } = volume;
 }
+
 
 public class Arrow(float weight, float volume) : InventoryItem(weight, volume)
 {
