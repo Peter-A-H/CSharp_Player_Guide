303a98aeef7c41de1e1d3bfeae4dd030d38e8240
Peter <peterhamblin@hotmail.co.uk>
2023-12-25 17:57:33 +0000

    Initial Commit

---
+++ The Old Robot/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public RobotCommand?[] Commands { get; } = new RobotCommand?[3];
+
+    public void Run()
+    {
+        foreach (RobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"{X} {Y} {IsPowered}");
+        }
+    }
+}
+
+public abstract class RobotCommand
+{
+    public void Run(Robot robot)
+    {
+
+    }
+}
