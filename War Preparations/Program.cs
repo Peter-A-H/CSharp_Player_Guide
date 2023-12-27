aac24c01da77d323b2478d32ec486edd16f6b94d
Peter <peterhamblin@hotmail.co.uk>
2023-12-27 15:13:36 +0000

    Challenge completed

---
+++ War Preparations/Program.cs
@@ -0,0 +1,13 @@
+﻿Sword basicSword = new(Material.Iron, Gem.None);
+Sword rareSword = basicSword with { Gemstone = Gem.Bitstone };
+Sword steelSword = basicSword with { Material = Material.Steel };
+Console.WriteLine(rareSword.ToString());
+
+public readonly record struct Sword(Material Material, Gem Gemstone)
+{
+    public int Length { get; } = 70;
+    public int Width { get; } = 7;
+}
+
+public enum Material { Wood, Bronze, Iron, Steel, Binarium }
+public enum Gem { Emerald, Amber, Sapphire, Diamond, Bitstone, None }
