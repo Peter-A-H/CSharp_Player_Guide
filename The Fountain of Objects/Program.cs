a48a24fedfec02584e411ff2039f5cfc2b90d8bc
Peter <peterhamblin@hotmail.co.uk>
2023-12-28 20:59:14 +0000

    Initial structure set

--- The Fountain of Objects/Program.cs
+++ The Fountain of Objects/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿public class Game
+{
+
+}
+
+public class RoomsGrid
+{
+    // create game grid
+}
+
+public class Room : RoomsGrid
+{
+    // represent individual rooms
+}
+
+public class Player
+{
+    public void Sense()
+    {
+        // player can sense what is in the next room
+    }
+
+    public void Action()
+    {
+        // move player
+    }
+}
+
