dc59c10c673f091cb714b062f79c95a19a87d064
Peter <peterhamblin@hotmail.co.uk>
2024-01-03 21:40:18 +0000

    Challenge completed

--- Safer Number Crunching/Program.cs
+++ Safer Number Crunching/Program.cs
@@ -1,18 +1,60 @@
-﻿static void ConvertNumber()
-{
-    Console.WriteLine("Enter a number.");
-    string? input = Console.ReadLine();
+﻿Parser.TryParseInt();
+Parser.TryParseDouble();
+Parser.TryParseBool();
 
-    if (int.TryParse(input, out int value))
+public static class Parser
+{
+    public static void TryParseInt()
     {
-        Console.WriteLine($"You entered the number {value}.");
+        Console.WriteLine("Enter a number.");
+        string? input = Console.ReadLine();
+
+        if (int.TryParse(input, out int value))
+        {
+            Console.WriteLine($"You entered the number {value}.");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("That is not a number.");
+            Console.WriteLine();
+            TryParseInt();
+        }
     }
-    else
+
+    public static void TryParseDouble()
     {
-        Console.WriteLine("That is not a number.");
-        Console.WriteLine();
-        ConvertNumber();
+        Console.WriteLine("Enter a double.");
+        string? input = Console.ReadLine();
+
+        if (double.TryParse(input, out double value))
+        {
+            Console.WriteLine($"You entered the double {value}.");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("That is not a double.");
+            Console.WriteLine();
+            TryParseDouble();
+        }
+    }
+
+    public static void TryParseBool()
+    {
+        Console.WriteLine("Enter a bool.");
+        string? input = Console.ReadLine();
+
+        if (bool.TryParse(input, out bool value))
+        {
+            Console.WriteLine($"You entered the bool {value}.");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("That is not a bool.");
+            Console.WriteLine();
+            TryParseBool();
+        }
     }
 }
-
-ConvertNumber();
