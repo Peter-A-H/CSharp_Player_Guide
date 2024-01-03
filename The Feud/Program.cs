7d927e63a8e48f87f5daaba67a87ea016df06c99
Peter <peterhamblin@hotmail.co.uk>
2024-01-03 19:13:08 +0000

    Challenge completed

---
+++ The Feud/Program.cs
@@ -0,0 +1,34 @@
+﻿using IField;
+using McDroid;
+
+Sheep sheep = new();
+IField.Pig pig = new();
+
+Cow cow = new();
+McDroid.Pig droidPig = new();
+
+namespace IField
+{
+    public class Sheep
+    {
+
+    }
+
+    public class Pig
+    {
+
+    }
+}
+
+namespace McDroid
+{
+    public class Cow
+    {
+
+    }
+
+    public class Pig
+    {
+
+    }
+}
