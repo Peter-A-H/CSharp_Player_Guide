02e67cbbf761a7a9621670ab49ad2ae98f505423
Peter <peterhamblin@hotmail.co.uk>
2024-01-03 21:30:46 +0000

    Function works for int values

--- Safer Number Crunching/Program.cs
+++ Safer Number Crunching/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿static void ConvertNumber()
+{
+    Console.WriteLine("Enter a number.");
+    string? input = Console.ReadLine();
+
+    if (int.TryParse(input, out int value))
+    {
+        Console.WriteLine($"You entered the number {value}.");
+    }
+    else
+    {
+        Console.WriteLine("That is not a number.");
+        Console.WriteLine();
+        ConvertNumber();
+    }
+}
+
+ConvertNumber();
