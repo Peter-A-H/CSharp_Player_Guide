8757297b0c6b5def1264a016efa2aec4740dc5b0
Peter <peterhamblin@hotmail.co.uk>
2024-01-04 21:24:23 +0000

    Challenge completed

---
+++ Better Random/Program.cs
@@ -0,0 +1,23 @@
+﻿Random random = new();
+Console.WriteLine(random.NextDouble(100));
+Console.WriteLine(random.NextString("my", "name", "is", "peter"));
+Console.WriteLine(random.CoinFlip());
+Console.WriteLine(random.CoinFlip(0.25));
+
+public static class RandomExtensions
+{
+    public static double NextDouble(this Random random, double max)
+    {
+        return random.NextDouble() * max;
+    }
+
+    public static string NextString(this Random random, params string[] input)
+    {
+        return input[random.Next(input.Length)];
+    }
+
+    public static bool CoinFlip(this Random random, double frequency = 0.5)
+    {
+        return random.NextDouble() < frequency;
+    }
+}
