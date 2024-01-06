2210d8224ce61d6e1719046879fc3615e2795301
Peter <peterhamblin@hotmail.co.uk>
2024-01-06 22:01:21 +0000

    Challenge completed

--- Exepti's Game/Program.cs
+++ Exepti's Game/Program.cs
@@ -11,62 +11,84 @@ public record Cookie
 public class Player(int playerType)
 {
     public int ChosenNumber { get; private set; }
-    public List<int> PreviousNumbers { get; private set; } = [];
+    public static List<int> PreviousNumbers { get; private set; } = [];
     public int PlayerType { get; private set; } = playerType;
 
     public void PickNumber()
     {
         Console.Write($"Player {PlayerType}, pick a number between 0 - 9: ");
         string? response = Console.ReadLine();
+        int chosenNumber = 0;
+
+        try
+        {
+            chosenNumber = Convert.ToInt32(response);
+        }
+        catch (FormatException exception)
+        {
+            Console.WriteLine(exception.Message);
+            PickNumber();
+        }
 
         if (response == null)
         {
-            Console.Write("Null value entered.\n");
+            Console.WriteLine("Null value entered.\n");
             PickNumber();
         }
 
-        if (PreviousNumbers.Contains(Convert.ToInt32(response)))
+        if (chosenNumber < 0 || chosenNumber > 9)
         {
-            Console.Write($"{response} has already been chosen before.\n");
+            Console.Write("Please enter a number between 0 - 9.\n");
             PickNumber();
         }
 
-        ChosenNumber = Convert.ToInt32(response);
+        if (PreviousNumbers.Contains(chosenNumber))
+        {
+            Console.WriteLine($"{response} has already been chosen before.\n");
+            PickNumber();
+        }
+
+        ChosenNumber = chosenNumber;
         PreviousNumbers.Add(ChosenNumber);
     }
 }
 
 public class CookieGame
 {
-    public bool InPlay { get; private set; } = true;
-    public Player Player1 { get; private set; } = new(1);
-    public Player Player2 { get; private set; } = new(2);
+    public static Player Player1 { get; private set; } = new(1);
+    public static Player Player2 { get; private set; } = new(2);
+    public Player CurrentPlayer { get; private set; } = Player1;
     public Cookie Cookie { get; private set; } = new();
 
     public void Run()
     {
-        while (InPlay)
+        while (true)
         {
-            Player1.PickNumber();
+            CurrentPlayer.PickNumber();
 
             try
             {
                 WasOatmealCookieChosen();
             }
-            catch (Exception e)
+            catch (OatmealCookieException exception)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(exception.Message);
+                Player winner = CurrentPlayer == Player1 ? Player2 : Player1;
+                Console.WriteLine($"Player {winner.PlayerType} has won the game!");
+                break;
             }
 
-            Console.WriteLine(Cookie.OatmealRaisin);
+            CurrentPlayer = CurrentPlayer == Player1 ? Player2 : Player1;
         }
     }
 
     public void WasOatmealCookieChosen()
     {
-        if (Player1.ChosenNumber == Cookie.OatmealRaisin)
+        if (CurrentPlayer.ChosenNumber == Cookie.OatmealRaisin)
         {
-            throw new Exception("Oatmeal raisin cookie was chosen!\n");
+            throw new OatmealCookieException("Oatmeal raisin cookie was chosen!");
         }
     }
 }
+
+public class OatmealCookieException(string message) : Exception(message) { }
