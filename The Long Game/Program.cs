c49705e32ec752adc5f7b70d5fa1979cc3ddd758
Peter <peterhamblin@hotmail.co.uk>
2024-01-12 19:08:48 +0000

    Completed challenge

--- The Long Game/Program.cs
+++ The Long Game/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Console.Write("Enter your name: ");
+string? name = Console.ReadLine();
+
+int score = 0;
+string fileName = $"{name}.txt";
+
+if (File.Exists(fileName))
+{
+    score = Convert.ToInt32(File.ReadAllText(fileName));
+    Console.WriteLine($"Welcome back, {name}. Your score will resume at {score}.");
+}
+
+while (true)
+{
+    ConsoleKey key = Console.ReadKey().Key;
+    if (key == ConsoleKey.Escape) break;
+    score++;
+    Console.WriteLine(score);
+}
+
+File.WriteAllText(fileName, score.ToString());
