819c49f5a54c146f26b8f89c2de91f6171b43c33
Peter <peterhamblin@hotmail.co.uk>
2024-01-13 20:44:24 +0000

    Challenge completed

--- Navigating Operand City/Program.cs
+++ Navigating Operand City/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿BlockCoordinate coords = new(4, 3);
+BlockOffset offset = new(2, 0);
+Console.WriteLine(coords + offset);
+Console.WriteLine(coords + Direction.North);
+
+public record BlockCoordinate(int Row, int Column)
+{
+    public static BlockCoordinate operator +(BlockCoordinate coordinate, BlockOffset offset)
+    {
+        return new BlockCoordinate(coordinate.Row + offset.RowOffset,
+                                   coordinate.Column + offset.ColumnOffset);
+    }
+
+    public static BlockCoordinate operator +(BlockCoordinate coordinate, Direction direction)
+    {
+        return coordinate + (direction switch
+        {
+            Direction.North => new BlockOffset(-1, 0),
+            Direction.South => new BlockOffset(+1, 0),
+            Direction.West => new BlockOffset(0, -1),
+            Direction.East => new BlockOffset(0, +1),
+            _ => throw new NotImplementedException()
+        });
+    }
+}
+
+public record BlockOffset(int RowOffset, int ColumnOffset);
+public enum Direction { North, East, South, West }
