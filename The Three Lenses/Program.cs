07d84cb9e227dc480a74c7a000ff92ff174f6327
Peter <peterhamblin@hotmail.co.uk>
2024-01-14 12:19:05 +0000

    Added base code for GameObject, Player and Ship

--- The Three Lenses/Program.cs
+++ The Three Lenses/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿List<GameObject> objects = [];
+objects.Add(new Ship
+{
+    ID = 1,
+    X = 0,
+    Y = 0,
+    HP = 50,
+    MaxHP = 100,
+    PlayerID = 1
+});
+objects.Add(new Ship
+{
+    ID = 2,
+    X = 4,
+    Y = 2,
+    HP = 75,
+    MaxHP = 100,
+    PlayerID = 1
+});
+objects.Add(new Ship
+{
+    ID = 3,
+    X = 9,
+    Y = 3,
+    HP = 0,
+    MaxHP = 100,
+    PlayerID = 2
+});
+
+List<Player> players = [];
+players.Add(new Player(1, "Player 1", "Red"));
+players.Add(new Player(1, "Player 2", "Blue"));
+
+public class GameObject
+{
+    public int ID { get; init; }
+    public double X { get; init; }
+    public double Y { get; init; }
+    public int MaxHP { get; init; }
+    public int HP { get; init; }
+    public int PlayerID { get; init; }
+}
+
+public class Ship : GameObject { }
+
+public record Player(int ID, string UserName, string TeamColor);
