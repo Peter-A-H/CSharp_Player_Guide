51a48dad467c876c72689e14dcc9791f6d35abfd
Peter <peterhamblin@hotmail.co.uk>
2024-01-15 19:59:04 +0000

    Challenge solution

--- The Repeating System/Program.cs
+++ The Repeating System/Program.cs
@@ -1,52 +1,55 @@
-﻿RecentNumbers recentNumbers = new();
-Thread thread = new(recentNumbers.Run);
-thread.Start();
+﻿RecentNumbers recentNumbers = new()
+{
+    MostRecent = -1,
+    SecondMostRecent = -2
+};
+Thread generatingThread = new(GenerateNumbers); // method to run
+generatingThread.Start(recentNumbers); // shared data to pass 
 
 while (true)
 {
-    recentNumbers.Run();
-    Console.WriteLine(string.Format("Here's the list from main: ({0}).", string.Join(", ", recentNumbers.NumbersList)));
+    Console.ReadKey(false);
+
+    bool isDuplicate;
+    lock (recentNumbers)
+    {
+        isDuplicate = recentNumbers.MostRecent == recentNumbers.SecondMostRecent;
+    }
+
+    if (isDuplicate)
+    {
+        Console.WriteLine("You found a duplicate!");
+    }
+    else
+    {
+        Console.WriteLine("That is not a duplicate.");
+    }
+}
+
+static void GenerateNumbers(object? obj)
+{
+    if (obj == null || obj is not RecentNumbers) return;
+
+    RecentNumbers recentNumbers = (RecentNumbers)obj;
+    Random random = new();
+
+    while (true)
+    {
+        int nextNumber = random.Next(10);
+
+        lock (recentNumbers)
+        {
+            recentNumbers.SecondMostRecent = recentNumbers.MostRecent;
+            recentNumbers.MostRecent = nextNumber;
+        }
+
+        Console.WriteLine(nextNumber);
+        Thread.Sleep(1000);
+    }
 }
 
 public class RecentNumbers
 {
-    private readonly object _numberLock = new();
-    private List<int> _recentNumbers = [];
-
-    public List<int> NumbersList
-    {
-        get
-        {
-            lock (_numberLock)
-            {
-                return _recentNumbers;
-            }
-        }
-    }
-
-    public void Run()
-    {
-        lock (_numberLock)
-        {
-            while (true)
-            {
-                Thread.Sleep(1000);
-                Random random = new();
-                int num = random.Next(10);
-
-                if (_recentNumbers.Count == 2)
-                {
-                    _recentNumbers[0] = _recentNumbers[1];
-                    _recentNumbers.Remove(_recentNumbers[1]);
-                    _recentNumbers.Add(num);
-                }
-                else
-                {
-                    _recentNumbers.Add(num);
-                }
-
-                Console.WriteLine(string.Format("Here's the list from thread: ({0}).", string.Join(", ", _recentNumbers)));
-            }
-        }
-    }
+    public int MostRecent { get; set; }
+    public int SecondMostRecent { get; set; }
 }
