70e5377300c998a66069234bfddddefa3a6d7960
Peter <peterhamblin@hotmail.co.uk>
2024-01-15 19:12:57 +0000

    Concurrency practice

---
+++ The Repeating System/Program.cs
@@ -0,0 +1,35 @@
+﻿SharedData sharedData = new();
+Thread thread = new(sharedData.Increment);
+thread.Start();
+
+sharedData.Increment();
+
+thread.Join();
+
+Console.WriteLine(sharedData.Number);
+
+class SharedData
+{
+    private readonly object _numberLock = new();
+
+    private int _number;
+
+    public int Number
+    {
+        get
+        {
+            lock (_numberLock)
+            {
+                return _number;
+            }
+        }
+    }
+
+    public void Increment()
+    {
+        lock (_numberLock)
+        {
+            _number++;
+        }
+    }
+}
