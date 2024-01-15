92a899c5e89efab59c232df1a1d59c2956c8ff45
Peter <peterhamblin@hotmail.co.uk>
2024-01-15 19:51:13 +0000

    First attempt

--- The Repeating System/Program.cs
+++ The Repeating System/Program.cs
@@ -1,35 +1,52 @@
-﻿SharedData sharedData = new();
-Thread thread = new(sharedData.Increment);
+﻿RecentNumbers recentNumbers = new();
+Thread thread = new(recentNumbers.Run);
 thread.Start();
 
-sharedData.Increment();
+while (true)
+{
+    recentNumbers.Run();
+    Console.WriteLine(string.Format("Here's the list from main: ({0}).", string.Join(", ", recentNumbers.NumbersList)));
+}
 
-thread.Join();
-
-Console.WriteLine(sharedData.Number);
-
-class SharedData
+public class RecentNumbers
 {
     private readonly object _numberLock = new();
+    private List<int> _recentNumbers = [];
 
-    private int _number;
-
-    public int Number
+    public List<int> NumbersList
     {
         get
         {
             lock (_numberLock)
             {
-                return _number;
+                return _recentNumbers;
             }
         }
     }
 
-    public void Increment()
+    public void Run()
     {
         lock (_numberLock)
         {
-            _number++;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Random random = new();
+                int num = random.Next(10);
+
+                if (_recentNumbers.Count == 2)
+                {
+                    _recentNumbers[0] = _recentNumbers[1];
+                    _recentNumbers.Remove(_recentNumbers[1]);
+                    _recentNumbers.Add(num);
+                }
+                else
+                {
+                    _recentNumbers.Add(num);
+                }
+
+                Console.WriteLine(string.Format("Here's the list from thread: ({0}).", string.Join(", ", _recentNumbers)));
+            }
         }
     }
 }
