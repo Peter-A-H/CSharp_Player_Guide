8f356e92234b4e18e41b346f4346c5e3a127e2ff
Peter <peterhamblin@hotmail.co.uk>
2024-01-22 22:24:37 +0000

    Challenge completed

--- The Great Humanizer/Program.cs
+++ The Great Humanizer/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using Humanizer;
+
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(30).Humanize()}");
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(2.5).Humanize()}");
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(50).Humanize()}");
