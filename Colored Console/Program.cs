d407527ec1947b0848c88246b4111e3c4e32245f
Peter <peterhamblin@hotmail.co.uk>
2024-01-22 22:11:22 +0000

    Challenge complete

--- Colored Console/Program.cs
+++ Colored Console/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using Color;
+
+string name = ColoredConsole.Prompt("What is your name? ");
+ColoredConsole.WriteLine($"Hello {name}", ConsoleColor.Green);
+Console.ForegroundColor = ConsoleColor.Gray;
