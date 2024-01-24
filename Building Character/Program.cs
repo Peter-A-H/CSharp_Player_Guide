62845b741cb9c1ebfec140b2215b115f02ea4311
Peter <peterhamblin@hotmail.co.uk>
2024-01-24 21:30:35 +0000

    Structure and design of the game implemented

--- Building Character/Program.cs
+++ Building Character/Program.cs
@@ -1,1 +1,57 @@
-﻿
+﻿public class UncodedOneGame
+{
+
+}
+
+public interface IAttackAction
+{
+    public byte Attack();
+}
+
+public class Party
+{
+
+}
+
+public class Character : Party
+{
+    public byte HP { get; set; }
+
+    public void DoNothing()
+    {
+    }
+}
+
+public class Hero : Character
+{
+
+}
+
+public class TrueProgrammer : Hero, IAttackAction
+{
+    public byte Attack()
+    {
+        byte damageAmount = 1;
+        return damageAmount;
+    }
+}
+
+public class Monster : Character
+{
+
+}
+
+public class Skeleton : Monster, IAttackAction
+{
+    public byte Attack()
+    {
+        Random random = new();
+        byte damageAmount = (byte)random.Next(0, 2);
+        return damageAmount;
+    }
+}
+
+public class UncodedOne : Monster
+{
+
+}
