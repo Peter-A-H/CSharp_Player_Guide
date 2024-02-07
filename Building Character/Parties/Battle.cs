16b812f95ecc76acb5fe136af42cef29c00ecacf
Peter <peterhamblin@hotmail.co.uk>
2024-02-07 19:38:59 +0000

    Game now ends when party is dead

--- Building Character/Parties/Battle.cs
+++ Building Character/Parties/Battle.cs
@@ -4,10 +4,11 @@ public class Battle(Party heroes, Party monsters)
 {
     public Party Heroes { get; } = heroes;
     public Party Monsters { get; } = monsters;
+    private bool _gamePlaying = true;
 
     public void Start()
     {
-        while (true)
+        while (_gamePlaying)
         {
             List<Party> parties = [Heroes, Monsters];
 
@@ -17,6 +18,20 @@ public class Battle(Party heroes, Party monsters)
 
                 foreach (Character character in party.Characters.ToList())
                 {
+                    if (Heroes.Characters.Count == 0)
+                    {
+                        Console.WriteLine("The enemy wins!");
+                        _gamePlaying = false;
+                        break;
+                    }
+
+                    if (Monsters.Characters.Count == 0)
+                    {
+                        Console.WriteLine("The Hero wins!");
+                        _gamePlaying = false;
+                        break;
+                    }
+
                     Console.WriteLine($"{character.Name} is taking a turn...");
                     party.Player.ChooseAction(this, character).RunAction(this, character);
                     Console.WriteLine();
